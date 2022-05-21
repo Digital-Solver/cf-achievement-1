4a9281c5bc490113614fb667092e98d95018b059
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-05-21 18:08:05 +0200

    Octals converted to strings
    ESLint doesn't like Octals (numbers beginning with 0) because they have caused problems in some JS applications, so I updated them to strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 let pokemonList = [
-    { id: 001, name: 'Bulbasaur', type: ['Grass', 'Poison'] },
-    { id: 002, name: 'Ivysaur', type: ['Grass', 'Poison'] },
-    { id: 003, name: 'Venusaur', type: ['Grass', 'Poison'] },
+    { id: '001', name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7 },
+    { id: '002', name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1.0 },
+    { id: '003', name: 'Venusaur', type: ['Grass', 'Poison'], height: 2.0 },
 
-    { id: 004, name: 'Charmander', type: ['Fire'] },
-    { id: 005, name: 'Charmeleon', type: ['Fire'] },
-    { id: 006, name: 'Charizard', type: ['Fire', 'Flying'] },
+    { id: '004', name: 'Charmander', type: ['Fire'], height: 0.6 },
+    { id: '005', name: 'Charmeleon', type: ['Fire'], height: 1.1 },
+    { id: '006', name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7 },
 
-    { id: 007, name: 'Squirtle', type: ['Water'] },
-    { id: 008, name: 'Wartortle', type: ['Water'] },
-    { id: 009, name: 'Blastoise', type: ['Water'] },
+    { id: '007', name: 'Squirtle', type: ['Water'], height: 0.5 },
+    { id: '008', name: 'Wartortle', type: ['Water'], height: 1.0 },
+    { id: '009', name: 'Blastoise', type: ['Water'], height: 1.6 },
 ];
