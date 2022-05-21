c816f1138b54801f0671690d48eb53cd4c6aa96f
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-05-21 17:44:55 +0200

    Fixed bugs in object properties
    Corrected types and updated string formatting for clearer reading

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 let pokemonList = [
-    { id: 001, name: Bulbasaur, type: ["grass", "poison"] },
-    { id: 002, name: Ivysaur, type: ["grass", "poison"] },
-    { id: 003, name: Venusaur, type: ["grass", "poison"] },
+    { id: 001, name: Bulbasaur, type: ["Grass", "Poison"] },
+    { id: 002, name: Ivysaur, type: ["Grass", "Poison"] },
+    { id: 003, name: Venusaur, type: ["Grass", "Poison"] },
 
-    { id: 004, name: Charmander, type: ["grass", "poison"] },
-    { id: 005, name: Charmeleon, type: ["grass", "poison"] },
-    { id: 006, name: Charizard, type: ["grass", "poison"] },
+    { id: 004, name: Charmander, type: ["Fire"] },
+    { id: 005, name: Charmeleon, type: ["Fire"] },
+    { id: 006, name: Charizard, type: ["Fire", "Flying"] },
 
-    { id: 007, name: Squirtle, type: ["grass", "poison"] },
-    { id: 008, name: Wartortle, type: ["grass", "poison"] },
-    { id: 009, name: Blastoise, type: ["grass", "poison"] },
+    { id: 007, name: Squirtle, type: ["Water"] },
+    { id: 008, name: Wartortle, type: ["Water"] },
+    { id: 009, name: Blastoise, type: ["Water"] },
 ];
