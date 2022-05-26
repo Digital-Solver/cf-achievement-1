fde4871f5852eb1d41d20e4ec60c863e85517c58
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-05-26 13:31:48 +0200

    Recovered the height property via Git revert
    I had accidentally deleted the height property from the pokemon objects, I recovered it through Git.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 let pokemonList = [
-    { id: 1, name: 'Bulbasaur', type: ['Grass', 'Poison'] },
-    { id: 2, name: 'Ivysaur', type: ['Grass', 'Poison'] },
-    { id: 3, name: 'Venusaur', type: ['Grass', 'Poison'] },
+    { id: 1, name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7 },
+    { id: 2, name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1.0 },
+    { id: 3, name: 'Venusaur', type: ['Grass', 'Poison'], height: 2.0 },
 
-    { id: 4, name: 'Charmander', type: ['Fire'] },
-    { id: 5, name: 'Charmeleon', type: ['Fire'] },
-    { id: 6, name: 'Charizard', type: ['Fire', 'Flying'] },
+    { id: 4, name: 'Charmander', type: ['Fire'], height: 0.6 },
+    { id: 5, name: 'Charmeleon', type: ['Fire'], height: 1.1 },
+    { id: 6, name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7 },
 
-    { id: 7, name: 'Squirtle', type: ['Water'] },
-    { id: 8, name: 'Wartortle', type: ['Water'] },
-    { id: 9, name: 'Blastoise', type: ['Water'] },
+    { id: 7, name: 'Squirtle', type: ['Water'], height: 0.5 },
+    { id: 8, name: 'Wartortle', type: ['Water'], height: 1.0 },
+    { id: 9, name: 'Blastoise', type: ['Water'], height: 1.6 },
 ];
 
 console.log(pokemonList);
