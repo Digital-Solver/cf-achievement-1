1bcc66f763e3a7b4567cffca13647252be46d8af
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-05-30 21:01:44 +0200

    Limited access to global scope by using an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
-// This list is the data source that will be looped through later in the code.
-let pokemonList = [
-    { id: 1, name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7 },
-    { id: 2, name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1.0 },
-    { id: 3, name: 'Venusaur', type: ['Grass', 'Poison'], height: 2.0 },
+// Pokemon repository as an immediately invoked function expression
+let pokemonRepository = (function () {
+    // The data list of Pokemon
+    let pokemonList = [
+        { id: 1, name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7 },
+        { id: 2, name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1.0 },
+        { id: 3, name: 'Venusaur', type: ['Grass', 'Poison'], height: 2.0 },
 
-    { id: 4, name: 'Charmander', type: ['Fire'], height: 0.6 },
-    { id: 5, name: 'Charmeleon', type: ['Fire'], height: 1.1 },
-    { id: 6, name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7 },
+        { id: 4, name: 'Charmander', type: ['Fire'], height: 0.6 },
+        { id: 5, name: 'Charmeleon', type: ['Fire'], height: 1.1 },
+        { id: 6, name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7 },
 
-    { id: 7, name: 'Squirtle', type: ['Water'], height: 0.5 },
-    { id: 8, name: 'Wartortle', type: ['Water'], height: 1.0 },
-    { id: 9, name: 'Blastoise', type: ['Water'], height: 1.6 },
-];
+        { id: 7, name: 'Squirtle', type: ['Water'], height: 0.5 },
+        { id: 8, name: 'Wartortle', type: ['Water'], height: 1.0 },
+        { id: 9, name: 'Blastoise', type: ['Water'], height: 1.6 },
+    ];
 
-// Loop goes through each object in the array and prints out its name and height to a new html tag
-let i = 0;
+    //The functions
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    // The Keys
+    return {
+        add: add,
+        getAll: getAll,
+    };
+})();
 
 for (i = 0; i < pokemonList.length; i++) {
     document.write(
