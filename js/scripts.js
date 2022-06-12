05d69ae1824bf1ad71bafd7146794197662959ba
Digital-Solver <105001360+Digital-Solver@users.noreply.github.com>
2022-06-12 13:39:13 +0200

    Made the pokedex load dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,8 @@ let pokemonRepository = (function () {
     // Pokemon repository as an immediately invoked function expression
 
     // **Data**
-    let pokemonList = [
-        { id: 1, name: 'Bulbasaur', type: ['Grass', 'Poison'], height: 0.7 },
-        { id: 2, name: 'Ivysaur', type: ['Grass', 'Poison'], height: 1.0 },
-        { id: 3, name: 'Venusaur', type: ['Grass', 'Poison'], height: 2.0 },
-
-        { id: 4, name: 'Charmander', type: ['Fire'], height: 0.6 },
-        { id: 5, name: 'Charmeleon', type: ['Fire'], height: 1.1 },
-        { id: 6, name: 'Charizard', type: ['Fire', 'Flying'], height: 1.7 },
-
-        { id: 7, name: 'Squirtle', type: ['Water'], height: 0.5 },
-        { id: 8, name: 'Wartortle', type: ['Water'], height: 1.0 },
-        { id: 9, name: 'Blastoise', type: ['Water'], height: 1.6 },
-    ];
+    let pokemonList = [];
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     // **Functions**
     function getAll() {
@@ -23,25 +12,46 @@ let pokemonRepository = (function () {
 
     function add(pokemon) {
         if (typeof pokemon === 'object') {
-            if (
-                Object.keys(pokemon)[0] === 'id' &&
-                Object.keys(pokemon)[1] === 'name' &&
-                Object.keys(pokemon)[2] === 'type' &&
-                Object.keys(pokemon)[3] === 'height'
-            ) {
-                pokemonList.push(pokemon);
-            } else {
-                alert(
-                    `Pokemon object must follow this format:\n
-                    id,name,type,height\n
-                    Your object has the following format:\n
-                    ${Object.keys(pokemon)}`
-                );
-            }
+            pokemonList.push(pokemon);
         } else {
             console.log(Object.keys(pokemon));
             alert('This is not an Object.');
         }
+    }
+
+    function loadList() {
+        return fetch(apiURL)
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                json.results.forEach(function (item) {
+                    let pokemon = {
+                        name: item.name,
+                        details: item.url,
+                    };
+                    add(pokemon);
+                });
+            })
+            .catch((error) => {
+                return error.message;
+            });
+    }
+
+    function loadDetails(item) {
+        let url = item.url;
+        return fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((details) => {
+                item.imageURL = details.spriates.front_default;
+                item.height = details.height;
+                item.types = details.types;
+            })
+            .catch((e) => {
+                return console.log(e.message);
+            });
     }
 
     function find(query) {
@@ -62,16 +72,18 @@ let pokemonRepository = (function () {
         }
     }
 
-    function showDetails(pokemon) {
-        console.log(pokemon.name);
-    }
-
     function addListItem(pokemon) {
+        function showDetails(pokemon) {
+            loadDetails(pokemon).then(() => console.log(pokemon));
+            console.log(pokemon.name);
+        }
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         button.innerText = pokemon.name;
+
         button.classList.add('poke-button');
+
         button.addEventListener('click', showDetails.bind(null, pokemon));
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
@@ -79,16 +91,16 @@ let pokemonRepository = (function () {
 
     // **Keys**
     return {
-        add: add,
         getAll: getAll,
+        add: add,
+        loadList: loadList,
+        loadDetails: loadDetails,
         find: find,
         addListItem: addListItem,
     };
 })();
 
-pokemonRepository.add({ id: 99, name: 'Mew', type: ['Psychic'], height: 0.2 }); // Add a pokemon dynamically
-
-pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.getAll().forEach((pokemon) => {
     // Print the pokemon details to DOM
     pokemonRepository.addListItem(pokemon);
 });
@@ -110,3 +122,7 @@ let searchPokedex = (function (query) {
 
 let searchBtn = document.getElementsByClassName('pokedex__search-button')[0]; // Event listener will search the pokedex
 searchBtn.addEventListener('click', searchPokedex.search);
+
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach((item) => pokemonRepository.addListItem(item));
+});
